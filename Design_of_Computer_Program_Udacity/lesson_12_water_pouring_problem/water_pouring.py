9240a6589758ccaf332c921ebf470dc995a2596f
joey_lau <lorgerd@163.com>
2019-06-23 21:20:39 +0800

    add lesson 12.

--- Design_of_Computer_Program_Udacity/lesson_12_water_pouring_problem/water_pouring.py
+++ Design_of_Computer_Program_Udacity/lesson_12_water_pouring_problem/water_pouring.py
@@ -4,3 +4,28 @@
  """
 __author__ = 'liujianhan'
 
+
+def pour_problem(X, Y, goal, start=(0, 0)):
+    """X and Y are the capacity of glasses; (x, y) is current fill levels
+    and represents a state. The goal is a level that can be in either glass.
+    Start at start state and follow successors until we reach the goal.
+    Keep track of frontier and previously explored; fail when no frontier."""
+    if goal in start:
+        return [start]
+    explored = set()  # set of states we have visited
+    frontier = [[start]]  # ordered list of paths we have blazed
+    while frontier:
+        path = frontier.pop(0)
+        (x, y) = path[-1]  # Last state in the first path of the frontier
+        for (state, action) in successors(x, y, X, Y).items():
+            if state not in explored:
+                explored.add(state)
+                path2 = path + [action, state]
+                if goal in state:
+                    return path2
+                else:
+                    frontier.append(path2)
+    return Fail
+
+
+Fail = []
