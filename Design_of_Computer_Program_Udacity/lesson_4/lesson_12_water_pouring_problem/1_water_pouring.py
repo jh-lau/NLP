99ed6400fa5a7848c6b57932552415f4a96569f8
joey_lau <lorgerd@163.com>
2019-06-26 18:09:54 +0800

    Modify lesson_4.

--- Design_of_Computer_Program_Udacity/lesson_4/lesson_12_water_pouring_problem/1_water_pouring.py
+++ Design_of_Computer_Program_Udacity/lesson_4/lesson_12_water_pouring_problem/1_water_pouring.py
@@ -6,6 +6,15 @@ __author__ = 'liujianhan'
 import doctest
 
 
+def successors(x, y, X, Y):
+    """Return a dict of {state: action} pairs describing what can be reached from the
+    (x, y) state, and how."""
+    assert x <= X and y <= Y
+    return {((0, y + x) if y + x <= Y else (x - (Y - y), y + (Y - y))): 'X->Y',
+            ((x + y, 0) if x + y <= X else (x + (X - x), y - (X - x))): 'Y->X',
+            (X, y): 'fill X', (x, Y): 'fill Y', (0, y): 'empty X', (x, 0): 'empty Y'}
+
+
 def pour_problem(X, Y, goal, start=(0, 0)):
     """X and Y are the capacity of glasses; (x, y) is current fill levels
     and represents a state. The goal is a level that can be in either glass.
@@ -30,15 +39,6 @@ def pour_problem(X, Y, goal, start=(0, 0)):
 
 
 Fail = []
-
-
-def successors(x, y, X, Y):
-    """Return a dict of {state: action} pairs describing what can be reached from the
-    (x, y) state, and how."""
-    assert x <= X and y <= Y
-    return {((0, y + x) if y + x <= Y else (x - (Y - y), y + (Y - y))): 'X->Y',
-            ((x + y, 0) if x + y <= X else (x + (X - x), y - (X - x))): 'Y->X',
-            (X, y): 'fill X', (x, Y): 'fill Y', (0, y): 'empty X', (x, 0): 'empty Y'}
 
 
 class Test: """
--- Design_of_Computer_Program_Udacity/lesson_4/lesson_12_water_pouring_problem/2_bridge_problem.py
+++ Design_of_Computer_Program_Udacity/lesson_4/lesson_12_water_pouring_problem/2_bridge_problem.py
@@ -3,6 +3,7 @@
   Time: 21:55
  """
 __author__ = 'liujianhan'
+import doctest
 
 
 def bridge_successors(state):
@@ -30,6 +31,25 @@ def bridge_successors(state):
                     for b in there if b is not 'light')
 
 
+def bridge_successors_2(state):
+    """Return a dict of {state:action} pairs. A state is a
+    (here, there) tuple, where here and there are frozensets
+    of people (indicated by their travel times) and/or the light."""
+    here, there = state
+    if 'light' in here:
+        return dict(((here - frozenset([a, b, 'light']),
+                      there | frozenset([a, b, 'light'])),
+                     (a, b, '->'))
+                    for a in here if a is not 'light'
+                    for b in here if b is not 'light')
+    else:
+        return dict(((here | frozenset([a, b, 'light']),
+                      there - frozenset([a, b, 'light'])),
+                     (a, b, '<-'))
+                    for a in there if a is not 'light'
+                    for b in there if b is not 'light')
+
+
 def bridge_problem(here):
     here = frozenset(here) | frozenset(['light'])
     explored = set()  # set of states we have visited
@@ -39,16 +59,20 @@ def bridge_problem(here):
         return frontier[0]
     while frontier:
         path = frontier.pop(0)
+        here_1, there_1, t1 = state_1 = path[-1]
+        if not here_1 or here_1 == {'light'}:  # Check for solution when we pull best path
+            return path
         for (state, action) in bridge_successors(path[-1]).items():
             if state not in explored:
                 here, there, t = state
                 explored.add(state)
                 path2 = path + [action, state]
-                if not here:  # That is, nobody left here
-                    return path2
-                else:
-                    frontier.append(path2)
-                    frontier.sort(key=elapsed_time)
+                # Don't check for solution when extend a path
+                # if not here:  # That is, nobody left here
+                #     return path2
+                # else:
+                frontier.append(path2)
+                frontier.sort(key=elapsed_time)
     return []
 
 
@@ -117,11 +141,49 @@ def test():
     assert bridge_successors((frozenset([]), frozenset([2, 'light']), 0)) == {
         (frozenset([2, 'light']), frozenset([]), 2): (2, 2, '<-')}
 
+    here1 = frozenset([1, 'light'])
+    there1 = frozenset([])
+
+    here2 = frozenset([1, 2, 'light'])
+    there2 = frozenset([3])
+
+    assert bridge_successors_2((here1, there1)) == {
+        (frozenset([]), frozenset([1, 'light'])): (1, 1, '->')}
+
+    assert bridge_successors_2((here2, there2)) == {
+        (frozenset([1]), frozenset(['light', 2, 3])): (2, 2, '->'),
+        (frozenset([2]), frozenset([1, 3, 'light'])): (1, 1, '->'),
+        (frozenset([]), frozenset([1, 2, 3, 'light'])): (2, 1, '->')}
+
     return 'tests pass'
 
 
+class TestBridge: """
+>>> elapsed_time((bridge_problem([1, 2, 5, 10])))
+17
+
+>>> [elapsed_time(bridge_problem([1, 2, 4, 8, 16][:n])) for n in range(6)]
+[0, 1, 2, 7, 15, 28]
+
+>>> [elapsed_time(bridge_problem([1,1,2,3,5,8,13,21][:n])) for n in range(8)]
+[0, 1, 1, 2, 6, 12, 19, 30]
+
+>>> S1 = [(2, 1, '->'), (1, 1, '<-'), (5, 10, '->'), (2, 2, '<-'), (2, 1, '->')]
+>>> S2 = [(2, 1, '->'), (2, 2, '<-'), (5, 10, '->'), (1, 1, '<-'), (2, 1, '->')]
+>>> path_actions(bridge_problem([1,2,5,10])) in (S1, S2)
+True
+
+>>> path_actions(bridge_problem([1,2,5,10,15,20]))
+[(2, 1, '->'), (1, 1, '<-'), (10, 5, '->'), (2, 2, '<-'), (2, 1, '->'), (1, 1, '<-'), (15, 20, '->'), (2, 2, '<-'), (2, 1, '->')]
+
+>>> path_actions(bridge_problem([1,2,4,8,16,32]))
+[(2, 1, '->'), (1, 1, '<-'), (8, 4, '->'), (2, 2, '<-'), (2, 1, '->'), (1, 1, '<-'), (16, 32, '->'), (2, 2, '<-'), (2, 1, '->')]
+"""
+
+
 if __name__ == '__main__':
-    # print(test())
+    print(test())
     # print(path_test())
-    print(bridge_problem([1, 2, 5, 10]))
-    print(bridge_problem([1, 2, 5, 10])[1::2])
+    # print(bridge_problem([1, 2, 5, 10]))
+    # print(bridge_problem([1, 2, 5, 10])[1::2])
+    # print(doctest.testmod())
