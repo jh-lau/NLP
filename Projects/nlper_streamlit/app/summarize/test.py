340361ed2903a9c469a9f48d42b1e0ba44194712
joey_lau <lorgerd@163.com>
2019-11-11 01:19:47 +0800

    update project files.

--- Projects/nlper_streamlit/app/summarize/test.py
+++ Projects/nlper_streamlit/app/summarize/test.py
@@ -4,6 +4,7 @@
  """
 __author__ = 'liujianhan'
 import time
+from bosonnlp import BosonNLP
 for i in range(1000):
     print("\r", f"{i}", end='', flush=True)
     time.sleep(.01)
---
+++ Projects/scrapy_demo/spider_demo/spider_demo/main.py
@@ -0,0 +1,11 @@
+"""
+  User: Liujianhan
+ """
+__author__ = 'liujianhan'
+
+import sys
+import os
+from scrapy.cmdline import execute
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+execute(['scrapy', 'crawl', 'antifraud'])
