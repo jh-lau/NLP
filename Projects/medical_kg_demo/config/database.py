deeb644ef2c7a2b531d96bac229ac4efeb34c86c
joey_lau <lorgerd@163.com>
2019-11-21 00:01:51 +0800

    update opened files.

---
+++ Projects/medical_kg_demo/config/database.py
@@ -0,0 +1,8 @@
+"""
+  User: Liujianhan
+ """
+__author__ = 'liujianhan'
+
+HOST = 'http://39.100.3.165:7474'
+USER = 'neo4j'
+PASSWORD = 'django'
---
+++ Projects/nlper_streamlit/app/medical_chatbot/config/database.py
@@ -0,0 +1,9 @@
+"""
+  User: Liujianhan
+ """
+__author__ = 'liujianhan'
+
+HOST = 'http://39.100.3.165:7474'
+# HTTP_PORT = 7474
+USER = 'neo4j'
+PASSWORD = 'django'
