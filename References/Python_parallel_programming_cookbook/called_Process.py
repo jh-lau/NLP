9f9d5c389ad6355495bd6ab84d88f3f9e2ffeb86
joey_lau <lorgerd@163.com>
2019-12-11 01:21:17 +0800

    update reference demo files.

---
+++ References/Python_parallel_programming_cookbook/called_Process.py
@@ -0,0 +1,3 @@
+print('Hello Python Parallel Cookbook!!')
+closeInput = input('Press Enter to exit')
+print('Closing calledProgress')
---
+++ References/Python_parallel_programming_cookbook/calling_Process.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+program = 'python'
+print('Process calling')
+arguments = ['called_Process.py']
+
+os.execvp(program, (program,) + tuple(arguments))
+print('Good Bye!')
